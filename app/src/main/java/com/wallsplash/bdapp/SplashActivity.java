f1259c72bea8a95fa6344c19ec7a8429767fe1b2
Abdullah-563014 <md.abdullah563014@gmail.com>
2019-11-03 01:22:14 +0600

    fjksj;

--- app/src/main/java/com/wallsplash/bdapp/SplashActivity.java
+++ app/src/main/java/com/wallsplash/bdapp/SplashActivity.java
@@ -1,12 +1,20 @@
 package com.wallsplash.bdapp;
 
+import android.Manifest;
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.wallsplash.bdapp.wallsplash.R;
@@ -40,8 +48,17 @@ public class SplashActivity extends AppCompatActivity {
         public void run() {
             try {
                 Thread.sleep(3500);
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
-                finish();
+                if (Build.VERSION.SDK_INT >= 23) {
+                    int PERMISSION_ALL = 1;
+                    String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                    if (!hasPermission(permissions)) {
+                        ActivityCompat.requestPermissions(SplashActivity.this, permissions, PERMISSION_ALL);
+                    } else {
+                        gotoMainActivity();
+                    }
+                } else {
+                    gotoMainActivity();
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -62,5 +79,33 @@ public class SplashActivity extends AppCompatActivity {
         imageView=findViewById(R.id.splashActivityImageViewId);
         welcomeTextView=findViewById(R.id.splashActivityWelcomeTextViewId);
         greetingTextView=findViewById(R.id.splashActivityGreetingViewId);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                gotoMainActivity();
+            } else {
+                Toast.makeText(this, "Permission denied, Please accept permission to download this file in your storage.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public boolean hasPermission(String... permissions) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(SplashActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void gotoMainActivity(){
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        finish();
     }
 }
